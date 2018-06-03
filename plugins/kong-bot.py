dd018b2924fb61a6540f223152a838f063dfadef
Unknown <QWxpY2UgUmFiYml0@gmail.com>
2018-06-03 01:12:04 -0400

    !help plugin added
    Kong-Bot now responds to pleas for help

--- plugins/kong-bot.py
+++ plugins/kong-bot.py
@@ -5,3 +5,8 @@ class TutorialPlugin(Plugin):
     @Plugin.command('ping')
     def command_ping(self, event):
         event.msg.reply('Pong!')
+
+class Help(Plugin):
+    @Plugin.command('!help')
+    def send_help(self, event):
+        event.msg.reply("To hear me say dumbass monkey lines, blurt out Christian's favorite synonym for apes.")
